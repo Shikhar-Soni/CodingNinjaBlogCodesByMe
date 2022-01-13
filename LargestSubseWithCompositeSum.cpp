12a0bf4e568ef7967b7ca030bc2f6856ac8838fc
Shikhar Soni <76754168+Shikhar-Soni@users.noreply.github.com>
2022-01-13 21:56:11 +0530

    Update LargestSubseWithCompositeSum.cpp

--- LargestSubseWithCompositeSum.cpp
+++ LargestSubseWithCompositeSum.cpp
@@ -21,15 +21,16 @@ void LargestSubsetWithCompositeSum(vector<int> a, int n){
     
     //edge case when sum <= 2
     if(sum_of_elements <= 2){
-        //only 0 set size is possible
-        cout << 0 << endl;
-        // empty line to show no elements
-        cout << endl;
+        cout << -1 << endl;
+        return;
     }
     if(isPrime(sum_of_elements)){
         // remove one odd element and print
-        cout << n - 1 << endl;
         // print all elements except one odd element
+        if(n == 1){
+            cout << -1 << endl;
+            return;
+        }
         bool odd_found = -1;
         for(int i = 0; i < n; i++){
             if(a[i] % 2 == 1){
@@ -39,6 +40,22 @@ void LargestSubsetWithCompositeSum(vector<int> a, int n){
         }
         // if the sum is a prime number then it must have an odd number
         assert(odd_found != -1);
+        
+        if(sum_of_elements - a[odd_found] == 2){
+            // special case when there's one odd and 2 in the array
+            
+            // if the odd number is not prime then we print it
+            if(!isPrime(a[odd_found])){
+                cout << 1 << endl;
+                cout << a[odd_found] << endl;
+            }
+            else{
+                cout << -1 << endl;
+            }
+            return;
+        }
+        
+        cout << n - 1 << endl;
         
         // print all elements except the one at index odd_found
         for(int i = 0; i < odd_found; i++){
